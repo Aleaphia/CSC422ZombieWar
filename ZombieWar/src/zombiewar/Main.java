6ecc9e9894264208d27ce0ffe196dd139981a27a
Wyatt Bechtle <bechtlew@csp.edu>
2024-12-01 14:58:32 -0600

    Changed the print statements to reflect release 2.0 requirements. Added dynamic labeling to the print statements. Added attributes to track the count of characters to the ZombieWarController.

---
+++ ZombieWar/src/zombiewar/Main.java
@@ -0,0 +1,9 @@
+package zombiewar;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ZombieWarController zWarCon = new ZombieWarController();
+        zWarCon.simulateZombieWar();
+    }
+}
